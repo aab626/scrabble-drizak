7da3631b276bd1003ef1e410bd5af3ffc807c31d
Augusto Aguayo <hostile.command@gmail.com>
2021-07-25 19:48:55 -0400

    added a common superclass exception for scrabble exceptions

--- src/main/java/cl/uchile/dcc/scrabble/exceptions/ASTOperationException.java
+++ src/main/java/cl/uchile/dcc/scrabble/exceptions/ASTOperationException.java
@@ -1,7 +1,7 @@
 package cl.uchile.dcc.scrabble.exceptions;
 
 /** Thrown when two operands in an AST cannot be evaluated with a certain operation. */
-public class ASTOperationException extends Exception {
+public class ASTOperationException extends ScrabbleException {
     public ASTOperationException(String msg){
         super(msg);
     }
--- src/main/java/cl/uchile/dcc/scrabble/exceptions/VariableNotFoundException.java
+++ src/main/java/cl/uchile/dcc/scrabble/exceptions/VariableNotFoundException.java
@@ -1,7 +1,7 @@
 package cl.uchile.dcc.scrabble.exceptions;
 
 /** Thrown when there was an attempt to fetch a variable with an unused name inside a varMap object */
-public class VariableNotFoundException extends Exception{
+public class VariableNotFoundException extends ScrabbleException {
     public VariableNotFoundException(String msg){
         super(msg);
     }
